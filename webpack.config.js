2469f36b52ca7ea763f43f2d69f5b50594342aaf
TokuyamaR <qpflapper.1007@gmail.com>
2021-05-24 08:49:41 +0900

    add style-loader and setting

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
       {
         test: /\.css/,
         use: [
+          // moduleはしたから順に適用される(style-loaderは必ずcss-laoderの上に記述する)
+          {
+            loader: "style-loader", //cssファイルを適用するためのモジュール(正確にはHTMLの中にスタイルタグを注入する役割を担っている)
+          },
           {
             loader: "css-loader", //jsファイルにcssファイルを読み込ませるためのモジュール
           },
