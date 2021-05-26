31413098375c8a76a8781b26c3ed6cec478897ac
TokuyamaR <qpflapper.1007@gmail.com>
2021-05-26 23:58:54 +0900

    install file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg)$/i,
         use: [
           {
-            loader: "url-loader",
+            loader: "file-loader",
             options: {
               esModule: false,
+              name: "images/sample_img.jpg",
             },
           },
         ],
