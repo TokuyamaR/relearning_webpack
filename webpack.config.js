138a4a27df3ebaf8263e6f90d1b0852c9552d4da
TokuyamaR <qpflapper.1007@gmail.com>
2021-05-29 09:25:00 +0900

    add access.pug file

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,15 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "./stylesheets/main.css", // jsにimportされたcssをcssファイルを切り離して自動生成する
     }),
-    new HtmlWebpackPlugin({ template: "src/templates/index.pug" }), // templateにビルドされたJS, cssファイルが全て読み込まれる
+    // templateにビルドされたJS, cssファイルが全て読み込まれる
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.pug",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/templates/access.pug",
+      filename: "access.html",
+    }),
     new CleanWebpackPlugin(), // outputディレクトリ配下にて、自動生成されるファイル以外の不要ファイルを削除する
   ],
 };
